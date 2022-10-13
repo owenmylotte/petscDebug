7bee9b2337717b76cffe3863853f8f648253420d
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2022-10-13 18:30:41 -0400

    more debug

--- example.c
+++ example.c
@@ -54,13 +54,13 @@ int main(int argc, char **argv)
     PetscReal* coord;                   //!< Pointer to the coordinate field information
     struct Virtualcoord* virtualcoord;  //!< Pointer to the primary (virtual) coordinate field information
     struct Identifier* identifier;      //!< Pointer to the ray identifier information
-    PetscCall(DMSwarmGetField(swarmDm, DMSwarmPICField_coor, NULL, NULL, (void**)&coord));
-    PetscCall(DMSwarmGetField(swarmDm, "identifier", NULL, NULL, (void**)&identifier));
-    PetscCall(DMSwarmGetField(swarmDm, "virtualCoord", NULL, NULL, (void**)&virtualcoord));
+//    PetscCall(DMSwarmGetField(swarmDm, DMSwarmPICField_coor, NULL, NULL, (void**)&coord));
+//    PetscCall(DMSwarmGetField(swarmDm, "identifier", NULL, NULL, (void**)&identifier));
+//    PetscCall(DMSwarmGetField(swarmDm, "virtualCoord", NULL, NULL, (void**)&virtualcoord));
 
-    PetscCall(DMSwarmRestoreField(swarmDm, DMSwarmPICField_coor, NULL, NULL, (void**)&coord));
-    PetscCall(DMSwarmRestoreField(swarmDm, "identifier", NULL, NULL, (void**)&identifier));
-    PetscCall(DMSwarmRestoreField(swarmDm, "virtualCoord", NULL, NULL, (void**)&virtualcoord));
+//    PetscCall(DMSwarmRestoreField(swarmDm, DMSwarmPICField_coor, NULL, NULL, (void**)&coord));
+//    PetscCall(DMSwarmRestoreField(swarmDm, "identifier", NULL, NULL, (void**)&identifier));
+//    PetscCall(DMSwarmRestoreField(swarmDm, "virtualCoord", NULL, NULL, (void**)&virtualcoord));
 
 
     PetscCall(DMSwarmMigrate(swarmDm, PETSC_TRUE));
