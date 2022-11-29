80f3471adbdc4897de8c71aee1ba964bf66aeacf
owenmylotte <owenmylotte@gmail.com>
2022-11-29 17:24:30 -0500

    Somewhat functional

--- gmshCellDebug.c
+++ gmshCellDebug.c
@@ -1,10 +1,13 @@
 static const char help[] = "Tests DMPlex gmsh compatability";
 
 #include <petscdmplex.h>
+#include "petscdm.h"
+#include <petsc.h>
 
 int main(int argc, char **argv) {
     DM dm, dma = NULL, dmDist = NULL;
-    DMLabel adaptLabel;
+
+    PetscInt dim = 3;
 
     PetscCall(PetscInitialize(&argc, &argv, NULL, help));
 
@@ -14,14 +17,72 @@ int main(int argc, char **argv) {
 
     PetscCall(DMViewFromOptions(dm, NULL, "-dm_view"));
 
-    // extrude based upon that label
-    PetscCall(DMAdaptLabel(dm, adaptLabel, &dma));
-    PetscCall(DMViewFromOptions(dma, NULL, "-dm_adapt_view"));
+    /** Get the indexes of all of the cells in the mesh
+     * These indexes can be read to produce valid centroid locations
+     * If DMLocatePoints fails to see any of these points, this demonstrates a bug.
+     * */
+    PetscInt start; //! Variables indicating the cell indexes of the mesh
+    PetscInt end;
+    const PetscInt *points;
 
-    // now distribute with the extruded cells
-    PetscCall(DMPlexDistribute(dma, 1, NULL, &dmDist));
+    PetscInt depth;
+    PetscCall(DMPlexGetDepth(dm, &depth));
+    IS allPointIS;
+    PetscCall(DMGetStratumIS(dm, "dim", depth, &allPointIS));
+    if (!allPointIS) PetscCall(DMGetStratumIS(dm, "depth", depth, &allPointIS));
+    PetscCall(ISGetPointRange(allPointIS, &start, &end, &points));
 
-    PetscCall(DMLabelDestroy(&adaptLabel));
+    /** Iterate through all of the cells in the mesh
+     * Get the centroid of each cell in the mesh
+     * Use DMLocatePoints to get the cell index back out of that location
+     * If DMLocatePoints fails to find a cell in that location, throw an error.
+     * */
+    for (PetscInt c = start; c < end; ++c) {
+        const PetscInt iCell = points ? points[c] : c; //! Represents the cell index
+        PetscReal centroid[3];
+        /** Get the centroid of the cell at this index */
+        PetscCall(DMPlexComputeCellGeometryFVM(dm, iCell, PETSC_NULLPTR, centroid, PETSC_NULLPTR));
+
+        /** At this point the centroid pointer should contain a valid cell location */
+
+        Vec intersect;
+        PetscCall(VecCreate(PETSC_COMM_SELF, &intersect));  //!< Instantiates the vector
+        PetscCall(VecSetBlockSize(intersect, dim));
+        PetscCall(VecSetSizes(intersect, PETSC_DECIDE, dim));  //!< Set size
+        PetscCall(VecSetFromOptions(intersect));
+        PetscInt i[3] = {0, 1, 2};                   //!< Establish the vector here so that it can be iterated.
+
+        /** Get the centroid coordinates and put them into the vector */
+        PetscReal position[3] = {(centroid[dim + 0]),   //!< x component
+                                 (centroid[dim + 1]),   //!< y component
+                                 (centroid[dim + 2])};  //!< z component
+
+        /** This block creates the vector pointing to the cell whose index will be stored during the current loop */
+        VecSetValues(intersect, dim, i, position,
+                     INSERT_VALUES);  //!< Actually input the values of the vector (There are 'dim' values to input)
+
+        /** Loop through points to try to get the cell that is sitting on that point */
+        PetscSF cellSF = PETSC_NULLPTR;
+        PetscCall(DMLocatePoints(dm, intersect, DM_POINTLOCATION_NONE, &cellSF));
+
+        /** Get the cells that were found from DMLocatePoints */
+        const PetscSFNode *cell;
+        PetscInt nFound;
+        const PetscInt *found;
+        PetscSFGetGraph(cellSF, NULL, &nFound, &found, &cell);
+
+        if (nFound == 0)
+            printf("No cell was found at centroid! %i %f %f %f\n", iCell, centroid[0], centroid[1],
+                   centroid[2]);
+        if (nFound == 1 && cell[0].index != iCell) printf("Output index is not equal to input! %i %i\n", iCell, cell[0].index);
+
+        /** Cleanup from DMLocatePoints */
+        PetscCall(VecDestroy(&intersect));
+        PetscCall(PetscSFDestroy(&cellSF));
+    }
+
+    /** Clean up the objects */
+    PetscCall(ISDestroy(&allPointIS));
     PetscCall(DMDestroy(&dm));
     PetscCall(DMDestroy(&dma));
     PetscCall(PetscFinalize());
@@ -30,18 +91,7 @@ int main(int argc, char **argv) {
 
 /*TEST
 
- Case 2: 2D example with invalid corner cell
-   mpirun -n 4  ./gMshExtrude -dm_plex_filename /Users/mcgurn/scratch/ablateInputs/test/combustionChamber2d_boundary_label_unrefinedv3.msh  -dm_view "hdf5:dm.h5" -dm_adapt_view "hdf5:adaptDm.h5"   -dm_adaptor cellrefiner  -dm_plex_transform_type extrude -dm_plex_transform_extrude_thickness 0.01  -dm_plex_check_all  -dm_plex_transform_extrude_use_tensor 0 -dm_plex_gmsh_use_regions -dm_plex_gmsh_mark_vertices -dm_partition_view
+-dm_plex_filename /home/owen/CLionProjects/petscDebug/R1_5m.msh -dm_view "hdf5:dm.h5" -dm_adapt_view "hdf5:adaptDm.h5"
+-dm_plex_filename /home/owen/CLionProjects/petscDebug/CombustionChamberV5_unrefinedv1.1.msh -dm_view "hdf5:dm.h5" -dm_adapt_view "hdf5:adaptDm.h5"
 
-
- Case 3: 2D Example with distribute error
- mpirun -n 34  ./gMshExtrude -dm_plex_filename /Users/mcgurn/scratch/ablateInputs/test/combustionChamber2d_boundary_label_unrefinedv3.msh  -dm_view "hdf5:dm.h5" -dm_adapt_view "hdf5:adaptDm.h5"  -dm_distribute_overlap 1   -dm_adaptor cellrefiner  -dm_plex_transform_type extrude -dm_plex_transform_extrude_thickness 0.05  -dm_plex_check_all  -dm_plex_transform_extrude_use_tensor 0 -dm_plex_gmsh_use_regions -dm_plex_gmsh_mark_vertices -dm_partition_view
-
-
-
-
- // unused inputs
- -dm_label_view AdaptationLabel
-
- -dm_
 TEST*/
