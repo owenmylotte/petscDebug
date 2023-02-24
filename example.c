058b36add0996f3054dee71d9a6f01881b84a460
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2023-02-24 16:33:33 -0500

    updated example.c

--- example.c
+++ example.c
@@ -6,9 +6,9 @@ int main(int argc, char **argv) {
     PetscCall(PetscInitialize(&argc, &argv, NULL, help));
 
     PetscInt dimensions = 3;
-    PetscInt faces[3] = {2, 2, 2};
-    PetscReal lower[3] = {0.0, 0.0, 0};
-    PetscReal upper[3] = {1, 1, 1};
+    PetscInt faces[3] = {188, 42, 42};
+    PetscReal lower[3] = {-0.00053763440860215054, -0.00063499999999999993, -0.013335};
+    PetscReal upper[3] = {0.10053763440860215, 0.026034999999999999, 0.013335};
     DMBoundaryType bc[3] = {DM_BOUNDARY_NONE, DM_BOUNDARY_NONE, DM_BOUNDARY_NONE};
 
     DM dm;
